5687185444f74edff4e06a5d8eba8a3133cb57f2
Todd Norton <toddmnorton@live.com>
2020-02-13 19:10:03 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -11,8 +11,6 @@ def speak_to_grandma
     puts "I LOVE YOU TOO PUMPKIN!"
 end
 
-speak_to_grandma
-
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
